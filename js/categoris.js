6ca48f6c8f6390d1d4e90c3d8e3e57e558a25170
mohamed kamal <153460005+mohamedkamal-17@users.noreply.github.com>
2024-01-29 19:55:28 +0200

    Update categoris.js

--- js/categoris.js
+++ js/categoris.js
@@ -3,31 +3,32 @@ async function getAllCategoris() {
 
     const respons = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
     const data = await respons.json();
-    console.log(data.categories)
+    
     displyAllCategoris(data.categories);
 
 }
 
 function displyAllCategoris(data) {
- console.log(9797)
+
     box = ""
    
     data.forEach(element => {
          console.log(element.strCategoryThumb)
         box += `
 
-        <div class="col-md-3 c-card position-relative" data-categori-nam="${element.strCategory}"  >
+        <div class="col-lg-3 col-md-4 col-sm-6 c-card p-2 overflow-hidden text-center rounded-2" data-categori-nam="${element.strCategory}"  >
+        <div class="position-relative ">
                         <img src="${element.strCategoryThumb}"class="w-100" alt="">
-                        <div class="layr bg-danger-subtle opacity-75 d-flex flex-column justify-content-center align-items-center">
+                        <div class="layr text-black  bg-info-subtle opacity-75 d-flex flex-column justify-content-center align-items-center">
                             <h3>${element.strCategory}</h3>
                             <p class=" px-3 text-centar"> ${element.strCategoryDescription.slice(0, 100)}</p>
+                        </div>
                         </div>
                     </div>`
         
         
             }
     );
-       console.log(box)
     $(".categoris").html(box);
 }
 
@@ -35,7 +36,7 @@ $(".categoris").on("click", ".c-card", function(){
 
     const name = $(this).attr("data-categori-nam");
 
-    console.log(name)
+   
    
 
     getAllMealCategoris(name);
@@ -51,7 +52,7 @@ async function getAllMealCategoris(categoris){
         
     const respons = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoris}`)
     const data = await respons.json();
-    console.log(data)
+    
     displayMals(data.meals)
    
 
